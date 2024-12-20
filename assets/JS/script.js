e5b598a6f9ed8256a409d9b03243d6c69cf046db
Floffy9560 <you@example.comauvrayflorian@aol.com>
2024-12-20 19:46:55 +0100

    lentilles

---
+++ assets/JS/script.js
@@ -0,0 +1,6 @@
+let contact = document.getElementById("contact");
+console.log(contact);
+
+contact.addEventListener("click", (event) => {
+  alert(`image src="/assets/img/ametropie22rezize.jpg"`);
+});
